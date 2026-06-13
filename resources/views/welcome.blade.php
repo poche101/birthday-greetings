abae1e29a1514a055c5d7f7a22ea01c9aaa3e94f
Poche Tech <pochetechofficial@gmail.com>
2026-06-13 21:36:29 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -485,15 +485,7 @@
 
     <div class="hero-overlay" aria-hidden="true"></div>
     <div class="hero-glow" aria-hidden="true"></div>
-       <br>
-       <br>
-
-    <div class="ornament" style="animation:fadeInUp 1s ease 0.1s both;">
-        <div class="ornament-line"></div>
-        <div class="ornament-diamond"></div>
-        <div class="ornament-line"></div>
-    </div>
-
+       
      <br>
       <br>
     <p class="hero-eyebrow"> A Golden Jubilee Celebration ✦</p>
